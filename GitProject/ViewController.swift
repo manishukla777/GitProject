3b4f66b9d0ad7c8cb11a553777524c6371ad914b
Dev Mani Shukla <devmanishukla@Devs-MacBook-Pro.local>
2018-04-11 16:24:48 +0530

    Initial Commit

---
+++ GitProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitProject
+//
+//  Created by Dev Mani Shukla on 4/11/18.
+//  Copyright © 2018 Dev Mani Shukla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
